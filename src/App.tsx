cba1505efc8217a80b02e8e126cbea25e2a0f5b7
Amir-Ghanem <amirghanem34@gmail.com>
2025-02-11 16:01:03 +0200

    -

--- src/App.tsx
+++ src/App.tsx
@@ -129,7 +129,7 @@ export default function App() {
               <Footer />
             </>
           }
-        />
+        />  
 
         {/* Auth Routes */}
         <Route path="/admin/login" element={<LoginPage />} />
